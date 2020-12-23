e3280d98a7fe4bd59ee1989699ebc23aa0625498
Ammar <ammarraneez@gmail.com>
2020-12-24 01:46:47 +0530

    .

--- codesprint-site/src/components/sponsors/Sponsor.js
+++ codesprint-site/src/components/sponsors/Sponsor.js
@@ -1,39 +1,49 @@
 import React from 'react'
 import './Sponsor.css'
-import './slick/slick.css'
-import './slick/slick-theme.css'
+import Slider from "react-slick";
+
 
 function Sponsor() {
+    var settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    };
+
     return (
         <div className="sponsor">
             <h1 id="heading">Sponsors</h1>
-            <div>
-                <img src="images/Sponser.png" />
-            </div>
-            <div>
-                <img src="images/Sponser.png" />
-            </div>
-            <div>
-                <img src="images/Sponser.png" />
-            </div>
-            <div>
-                <img src="images/Sponser.png" />
-            </div>
-            <div>
-                <img src="images/Sponser.png" />
-            </div>
-            <div>
-                <img src="images/Sponser.png" />
-            </div>
-            <div>
-                <img src="images/Sponser.png" />
-            </div>
-            <div>
-                <img src="images/Sponser.png" />
-            </div>
-            <div>
-                <img src="images/Sponser.png" />
-            </div>
+            <Slider {...settings}>
+                <div>
+                    <img src="images/Sponser.png" />
+                </div>
+                <div>
+                    <img src="images/Sponser.png" />
+                </div>
+                <div>
+                    <img src="images/Sponser.png" />
+                </div>
+                <div>
+                    <img src="images/Sponser.png" />
+                </div>
+                <div>
+                    <img src="images/Sponser.png" />
+                </div>
+                <div>
+                    <img src="images/Sponser.png" />
+                </div>
+                <div>
+                    <img src="images/Sponser.png" />
+                </div>
+                <div>
+                    <img src="images/Sponser.png" />
+                </div>
+                <div>
+                    <img src="images/Sponser.png" />
+                </div>
+            </Slider>
         </div>
     )
 }
